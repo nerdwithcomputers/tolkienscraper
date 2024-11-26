d1ac2e51166b56f4c19f361e0e5de1551a156bc1
nerdwithcomputers <hpscigeek@gmail.com>
2024-11-26 17:27:31 -0500

    mebbe

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ import stealth from "puppeteer-extra-plugin-stealth";
 var app = express();
 app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, url");
     next();
 });
 
