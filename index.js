21b67ebd3ec062132479009d9d0f625958649987
nerdwithcomputers <hpscigeek@gmail.com>
2024-11-25 20:51:03 -0500

    lotsa things

--- index.js
+++ index.js
@@ -1,16 +1,20 @@
 import express from "express";
-import puppeteer from "puppeteer";
+import puppeteer from "puppeteer-extra";
 import cors from "cors";
+import stealth from "puppeteer-extra-plugin-stealth";
 
 var app = express();
 app.use(cors());
 const port = 3000;
+// sneaky boi
+puppeteer.use(stealth());
 
 async function scrape(url){
     const browser = await puppeteer.launch();
     const page = await browser.newPage();
     await page.goto(url);
     const data = await page.content();
+    // await page.screenshot({ path: 'screenshot.png' });
     // console.log(data);
     await browser.close();
     return data;
