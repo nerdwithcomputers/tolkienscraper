f629d4040b1f20be0e306dab34a3ae22931bfeb5
nerdwithcomputers <hpscigeek@gmail.com>
2024-11-26 17:18:55 -0500

    maybe works

--- index.js
+++ index.js
@@ -32,6 +32,13 @@ app.get('/', (req, res)=>{
     scrape(url).then((x)=> res.send(x));
 });
 
+app.options("/*", function(req, res, next){
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
+  res.send(200);
+});
+
 app.listen(port, ()=>{
     console.log('server listening on: ' + port.toString());
 });
