95a7511e5f2a5c10d10227f999a9942c9b170bcb
nerdwithcomputers <hpscigeek@gmail.com>
2024-11-26 17:29:17 -0500

    argh

--- index.js
+++ index.js
@@ -32,7 +32,7 @@ app.get('/', (req, res)=>{
     scrape(url).then((x)=> res.send(x));
 });
 
-app.options("/*", function(req, res, next){
+app.options("/", function(req, res, next){
   res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
   res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With, url');
