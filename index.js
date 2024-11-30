30a385f75984b40d6577584ff264c0ce8cc0c557
nerdwithcomputers <hpscigeek@gmail.com>
2024-11-29 19:47:03 -0500

    sync

--- index.js
+++ index.js
@@ -24,7 +24,8 @@ app.options("/", function(req, res, next){
     // res.header('Access-Control-Allow-Origin', '*');
     // res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
     // res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With, url');
-  res.send(200);
+    res.send(200);
+    next();
 });
 
 app.get('/', (req, res, next)=>{
