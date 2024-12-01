b5c3bd7e7c5b4ca5b3c39e0be608741bf7891406
nerdwithcomputers <hpscigeek@gmail.com>
2024-12-01 10:22:50 -0500

    eh?

--- index.js
+++ index.js
@@ -20,12 +20,12 @@ async function scrape(url){
     return data;
 }
 
-app.options("/", (req, res)=>{
+app.options("/", (req, res, next)=>{
     res.set('Access-Control-Allow-Origin', ['*']);
     res.set('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
     res.set('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With, url');
     res.status(200);
-    // next();
+    next();
 });
 
 app.get('/', (req, res)=>{
